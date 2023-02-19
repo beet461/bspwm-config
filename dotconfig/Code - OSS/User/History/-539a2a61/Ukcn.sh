19ac6aaef12124e687e09f6ac464f7b20f516fbc
beet461 <rushvithanthagiri@gmail.com>
2023-02-19 18:08:59 +0530

    added thunar image

--- dotconfig/Code - OSS/User/History/-539a2a61/Ukcn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THEME="lofi"
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=$(dirname $0)/$THEME/config.ini
-polybar main -c $CONFIG_DIR &
